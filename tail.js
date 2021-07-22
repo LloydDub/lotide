56f105d0fd1f39e8a65233d11a2f689c7f8eb932
Lloyd Worth <lloydrww@gmail.com>
2021-07-22 21:36:36 +0000

    refacted the code

--- tail.js
+++ tail.js
@@ -6,8 +6,7 @@ const assertEqual = function(actual, expected) {
   }
 };
 const tail = function(tailArray) {
-  let tailItem = tailArray.slice(1);
-  return tailItem;
+ return tailArray.slice(1);   
 };
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
