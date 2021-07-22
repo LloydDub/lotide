002f61919dfba7674930e239e582a1eaac0b6193
Lloyd Worth <lloydrww@gmail.com>
2021-07-22 19:24:00 +0000

    head method clone

---
+++ head.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(array) {
+  let headItem = array[0];
+  return headItem;  
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
