93f9be5b3f8c57e837844ac7b6630edd887554b1
Lloyd Worth <lloydrww@gmail.com>
2021-07-26 23:39:43 +0000

    Added length check

--- eqArrays.js
+++ eqArrays.js
@@ -7,19 +7,23 @@ const assertEqual = function(actual, expected) {
 };
 
 function eqArrays(array1, array2) {
+  if (array1 === array2) return true;
+  if (array1 == null || array2 == null) return false;
+  if (array1.length !== array2.length) return false;
 
-  for(let i = 0; i < array1.length; i++){
-    if(array1[i] !== array2[i]){
-      return false;
-    }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+    
   }
   return true;
- 
 }
 
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+
+  
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])) // => true
 // console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-// console.log(eqArrays(["1", "2", "3"], ["1", 2, 3])) // => false
-// 
+console.log(eqArrays(["1", "2", "3"], ["1", 2, 3])) // => false
+
