72681bd6047115ee653a4220e0335510e74e01f5
Lloyd Worth <lloydrww@gmail.com>
2021-07-26 20:30:50 +0000

    flatten clone

---
+++ without.js
@@ -0,0 +1,30 @@
+function eqArrays(array1, array2) {
+  for(let i = 0; i < array1.length; i++){
+    if(array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true; 
+}
+
+
+const assertArraysEqual = function(array1, array2) {
+  let result = eqArrays(array1, array2)
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);    
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }  
+}
+
+function without(source, itemsToRemove) {
+  
+  return source.filter(function(ele){
+    return ele != itemsToRemove;
+  })
+}
+
+const words = ["hello", "world", "lighthouse"];
+console.log(without(words, ["lighthouse"])); // no need to capture return value for this test case
+// // Make sure the original array was not altered by the without function
+// assertArraysEqual(words, ["hello", "world", "lighthouse"]);
