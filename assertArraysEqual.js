5820ec8b8b7d76a192cc79969baecee1650fc7dd
Lloyd Worth <lloydrww@gmail.com>
2021-07-26 18:42:51 +0000

    Fixed the repeated msg error

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,24 @@
+
 function eqArrays(array1, array2) {
 
   for(let i = 0; i < array1.length; i++){
     if(array1[i] !== array2[i]){
-      console.log(`🛑🛑🛑 Assertion Failed: ${array1[i]} !== ${array2[i]}`);
-    }else {
-      console.log(`✅✅✅ Assertion Passed: ${array1[i]} === ${array2[i]}`);
+      return false;
     }
   }
-  
-  
+  return true;
+ 
+}
+let assertArraysEqual = function (array1, array2) {
+  let result = eqArrays(array1, array2)
+  if (result === true) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);    
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
 
+  
 }
 
-eqArrays([1, 4, 6, 7 ],[1, 4, 6, 7 ])
+
+assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 7])
