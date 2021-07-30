03252625c6ac4458031d5518cd2430ce185be004
Lloyd Worth <lloydrww@gmail.com>
2021-07-30 02:37:11 +0000

    Returns indedx of string in an array

---
+++ letterPositions.js
@@ -0,0 +1,42 @@
+
+
+//console.log(character)
+//---------------------------------------------------------------\\
+
+// go throught the sentence then add to results.
+
+const letterPositions = function(sentence) {
+  const results = {};
+  // const countingArray = [];
+  
+   for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " "){
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);  //if we found the elelment in the array we add it on itself.
+        } else {
+          results[sentence[i]] = [i];
+        } 
+    }
+      
+  
+    
+
+     
+        
+ }
+      console.log(results);
+}
+                                                               
+    
+  
+    
+    
+    
+    
+   
+
+  
+
+
+letterPositions('hello there');
+
