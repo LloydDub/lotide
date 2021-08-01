1066788bc2ca271b56267dea9d8f2529e12827ae
Lloyd Worth <lloydrww@gmail.com>
2021-08-01 19:15:09 +0000

    Object equals checker

---
+++ assertObjectsEqual.js
@@ -0,0 +1,50 @@
+// EG OBJECTS
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1); // First I will check the length of first object by comparing these keys variables
+  const keys2 = Object.keys(object2); // object2 assigned to a second key variable.
+
+  if (keys1.length !== keys2.length) {
+    return false;
+    
+  }
+
+  for (const key of keys1) {
+    const val1 = object1[key];
+    const val2 = object2[key];
+    const areObjects = isObject(val1) && isObject(val2);
+    if (areObjects && !eqObjects(val1, val2) || !areObjects && val1 !== val2) {
+      return false;
+    }
+    
+  }
+ return true;
+
+}
+function isObject(object) {
+  return object != null && typeof object === 'object';
+}
+//------------------------------
+
+const assertObjectsEqual = function(actual, expected) {
+   const inspect = require('util').inspect;
+   if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)}) === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)}) !== ${inspect(expected)}`);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+
+
+console.log(assertObjectsEqual(ab, ba), true)
