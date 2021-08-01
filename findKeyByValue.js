0c4132ffacec83284905808c41724511398850ff
Lloyd Worth <lloydrww@gmail.com>
2021-08-01 19:16:10 +0000

    finds object key by value

---
+++ findKeyByValue.js
@@ -0,0 +1,7 @@
+
+const findKeyByValue = function(object, value) {
+  const keysArray = Object.keys(object);
+  for (let element of keysArray) {
+    if (object[element] === value) return element;
+  }
+};
