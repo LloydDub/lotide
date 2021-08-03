7bb75866e8986a33807295da72a57db853f744a1
Lloyd Worth <lloydrww@gmail.com>
2021-08-03 22:13:24 +0000

    find keys

---
+++ findKey.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+let findKey = (object, callback) => {
+  for (let i in object) { // loops over the objest
+    if (callback(object[i])) { // stops at truthy 
+      return i; // return i which is the key
+    };         
+  } ;   
+};
+
+
+
+ 
+
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), 'noma')
+
+
