7bdd8e7d21ac561f6553494e904781d1234cd56e
Lloyd Worth <lloydrww@gmail.com>
2021-08-14 22:49:15 +0000

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 function eqArrays(array1, array2) {
   if (array1 === array2) return true;
@@ -26,4 +20,4 @@ console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])) // => true
 
 // console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
 console.log(eqArrays(["1", "2", "3"], ["1", 2, 3])) // => false
-
+module.exports = eqArrays;
