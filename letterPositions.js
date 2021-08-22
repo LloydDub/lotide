957e4639f2af522f82d8fa49fd185aee07d1468a
Lloyd Worth <74573525+LloydDub@users.noreply.github.com>
2021-08-22 13:58:35 -0600

    Update letterPositions.js
    cleaned it up and fixed return bug

--- letterPositions.js
+++ letterPositions.js
@@ -1,36 +1,17 @@
-
-
-//console.log(character)
-//---------------------------------------------------------------\\
-
-// go throught the sentence then add to results.
-
 const letterPositions = function(sentence) {
   const results = {};
-  // const countingArray = [];
-  
-   for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " "){
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
       if (results[sentence[i]]) {
-        results[sentence[i]].push(i);  //if we found the elelment in the array we add it on itself.
-        } else {
-          results[sentence[i]] = [i];
-        } 
+        results[sentence[i]].push(i); //if we found the elelment in the array we add it on  itself.
+      } else {
+        results[sentence[i]] = [i];
+      }
     }
-      
-  
-    
-
-     
+  }
+  return results;
+};
         
- }
-      console.log(results);
-}
-                                                               
-    
-  
-    
-    
     
     
    
